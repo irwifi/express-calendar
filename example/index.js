b07f37ef0a207f2ea77d63e4c4b6a558d56402c3
winniehell <git@winniehell.de>
2016-03-31 13:58:36 +0200

    fetch events from Google Calendar

--- example/index.js
+++ example/index.js
@@ -1,7 +1,10 @@
 var express = require('express')
 var expressCalendar = require('..')
+var options = require('./options.local.json')
+
+options.parameters.fields = ['items/summary', 'items/id'].join(',')
 
 var app = express()
-expressCalendar(app)
+expressCalendar(app, options)
 
 module.exports = app
--- index.js
+++ index.js
@@ -1,11 +1,40 @@
+var https = require('https')
+
 module.exports = expressCalendar
 
-function expressCalendar (router) {
+function expressCalendar (router, options) {
   router.get('/:year(\\d{4})/', expressCalendarMiddleware)
   router.get('/:year(\\d{4})/:month(\\d{2})/', expressCalendarMiddleware)
   router.get('/:year(\\d{4})/:month(\\d{2})/:day(\\d{2})/', expressCalendarMiddleware)
+
+  function expressCalendarMiddleware (req, res, next) {
+    fetchEvents(options.calendarId, options.parameters, function (err, result) {
+      if (err) {
+        return next(err)
+      }
+
+      if (result.error) {
+        res.status(result.error.code)
+      } else {
+        result.date = req.params
+      }
+      res.json(result)
+    })
+  }
 }
 
-function expressCalendarMiddleware (req, res, next) {
-  res.json(req.params)
+function fetchEvents (calendarId, parameters, callback) {
+  var parameterString = Object.keys(parameters).map(function (key) {
+    return key + '=' + parameters[key]
+  }).join('&')
+
+  var url = 'https://www.googleapis.com/calendar/v3/calendars/' + calendarId + '/events?' + parameterString
+  https.get(url, function (calendarResponse) {
+    calendarResponse.on('data', function (responseData) {
+      var responseDataObject = JSON.parse(responseData.toString())
+      callback(null, responseDataObject)
+    })
+  }).on('error', function (e) {
+    callback(e)
+  })
 }
